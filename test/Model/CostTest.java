523b5af45e07adc42da2568bdf13351ae7d62a7b
MKKOW <56612182+MKKOW@users.noreply.github.com>
2020-05-28 11:12:00 +0200

    Final sprzątania

--- test/Model/CostTest.java
+++ test/Model/CostTest.java
@@ -3,7 +3,6 @@ package Model;
 import Exceptions.IllegalCostException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Tests for Cost class")
